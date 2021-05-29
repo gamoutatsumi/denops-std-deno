2952321659cd6bda0484e5100ef67560f5479596
Alisue <lambdalisue@hashnote.net>
2021-05-29 16:38:10 +0900

    Add testutils to help writing tests with Vim/Neovim

--- deps_test.ts
+++ deps_test.ts
@@ -1,1 +1,4 @@
 export * from "https://deno.land/std@0.95.0/testing/asserts.ts";
+
+export { Session } from "https://deno.land/x/msgpack_rpc@v2.8/mod.ts";
+export { Session as VimSession } from "https://deno.land/x/vim_channel_command@v0.4/mod.ts";
---
+++ testutils_test.ts
@@ -0,0 +1,22 @@
+import { assertEquals, Session, VimSession } from "./deps_test.ts";
+import { withNvim, withVim } from "./testutils.ts";
+
+Deno.test("withNvim start nvim to communicate with", async () => {
+  await withNvim(async (reader, writer) => {
+    const session = new Session(reader, writer);
+    session.listen();
+    const result = await session.call("nvim_eval", "1 + 1");
+    assertEquals(result, 2);
+    await session.notify("nvim_command", "qall!");
+  });
+});
+
+Deno.test("withVim start vim to communicate with", async () => {
+  await withVim(async (reader, writer) => {
+    const session = new VimSession(reader, writer);
+    session.listen();
+    const result = await session.expr("1 + 1");
+    assertEquals(result, 2);
+    await session.ex("qall!");
+  });
+});
